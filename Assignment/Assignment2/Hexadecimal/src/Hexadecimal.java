55b08c6ab0161d7472b5ae2c8d5a4ca22dee0d3d
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-07-20 11:54:26 +0530

    Hexadecimal

--- Assignment/Assignment2/Hexadecimal/src/Hexadecimal.java
+++ Assignment/Assignment2/Hexadecimal/src/Hexadecimal.java
@@ -2,119 +2,177 @@ import java.util.Scanner;
 
 public class Hexadecimal {
     private static Scanner sc = new Scanner(System.in);
-    public Hexadecimal(String hexa_first, String hexa_second) {
-        menuList(hexa_first, hexa_second);
+    public Hexadecimal(String hexafirst, String hexasecond) {
+        menuList(hexafirst, hexasecond);
     }
 
-    void menuList(String hexa_first, String hexa_second) {
+    /*
+     * for diplaying the menu option
+     * @param first hexadecimal input by user
+     * @param second hexadecimal input ub user
+     */
+    void menuList(String hexafirst, String hexasecond) {
         System.out.println("\n1. Add\n2. Substract\n3. Multiply\n4.Divide\n5. Is Hexa values are equal?\n6. Is First Hexa values is greater?\n7. Is Second Hexa values is greater?");
         System.out.print("Enter your choice : ");
         switch (sc.nextInt()) {
             case 1:
-                add(hexa_first, hexa_second);
+                add(hexafirst, hexasecond);
                 break;
             case 2:
-                sub(hexa_first, hexa_second);
+                sub(hexafirst, hexasecond);
                 break;
             case 3:
-                mul(hexa_first, hexa_second);
+                mul(hexafirst, hexasecond);
                 break;
             case 4:
-                div(hexa_first, hexa_second);
+                div(hexafirst, hexasecond);
                 break;
             case 5:
-                isHexaEqual(hexa_first, hexa_second);
+                isHexaEqual(hexafirst, hexasecond);
                 break;
             case 6:
-                isHexaFirstGreater(hexa_first, hexa_second);
+                isHexaFirstGreater(hexafirst, hexasecond);
                 break;
             case 7:
-                isHexaSecondGreater(hexa_first, hexa_second);
+                isHexaSecondGreater(hexafirst, hexasecond);
                 break;
             default:
                 System.out.print("There is no such choice");
         }
     }
 
-    int hexaToDecimal(String hexaValue) { //converting hexadecimal value to decimal value
-        return Integer.parseInt(hexaValue, 16);
+    /*
+     * converting hexadecimal value to decimal value
+     * @param hexadecimal value
+     * @return decimal value of respective hexadecimal
+     */
+    int hexaToDecimal(String hexavalue) { //
+        return Integer.parseInt(hexavalue, 16);
     }
 
-    String decimalToHexa(int decimalValue) { //converting decimal value to hexadecimal value
-        return Integer.toHexString(decimalValue);
+    /*
+     * converting decimal value to hexadecimal value
+     * @param  decimal  value
+     * @return hexadecimal value of respective decimal
+     */
+    String decimalToHexa(int decimalvalue) { //converting decimal value to hexadecimal value
+        return Integer.toHexString(decimalvalue);
     }
 
-    void add(String first_hexa_value, String second_hexa_value) { //adding to hexadecimal value
-        int first_value = hexaToDecimal(first_hexa_value);
-        int second_value = hexaToDecimal(second_hexa_value);
-        int result = first_value + second_value;
-        String result_hexa = decimalToHexa(result);
-        display(result_hexa);
+    /*
+     * add two hexadecimal number
+     * @param first hexadecimal number
+     * @param second hexadecimal number
+     */
+    void add(String firsthexavalue, String secondhexavalue) { //adding to hexadecimal value
+        int firstvalue = hexaToDecimal(firsthexavalue);
+        int secondvalue = hexaToDecimal(secondhexavalue);
+        int result = firstvalue + secondvalue;
+        String resulthexa = decimalToHexa(result);
+        display(resulthexa);
     }
 
-    void sub(String first_hexa_value, String second_hexa_value) { //subtracting to hexadecimal value
-        int first_value = hexaToDecimal(first_hexa_value);
-        int second_value = hexaToDecimal(second_hexa_value);
-        int result = first_value - second_value;
-        String result_hexa = decimalToHexa(result);
-        display(result_hexa);
+    /*
+     * subtract two hexadecimal number
+     * @param first hexadecimal number
+     * @param second hexadecimal number
+     */
+    void sub(String firsthexavalue, String secondhexavalue) { //subtracting to hexadecimal value
+        int firstvalue = hexaToDecimal(firsthexavalue);
+        int secondvalue = hexaToDecimal(secondhexavalue);
+        int result = firstvalue - secondvalue;
+        String resulthexa = decimalToHexa(result);
+        display(resulthexa);
     }
 
-    void mul(String first_hexa_value, String second_hexa_value) { //multiplying to hexadecimal value
-        int first_value = hexaToDecimal(first_hexa_value);
-        int second_value = hexaToDecimal(second_hexa_value);
-        int result = first_value * second_value;
-        String result_hexa = decimalToHexa(result);
-        display(result_hexa);
+    /*
+     * multiply two hexadecimal number
+     * @param first hexadecimal number
+     * @param second hexadecimal number
+     */
+    void mul(String firsthexavalue, String secondhexavalue) { //multiplying to hexadecimal value
+        int firstvalue = hexaToDecimal(firsthexavalue);
+        int secondvalue = hexaToDecimal(secondhexavalue);
+        int result = firstvalue * secondvalue;
+        String resulthexa = decimalToHexa(result);
+        display(resulthexa);
     }
 
-    void div(String first_hexa_value, String second_hexa_value) { //divide to hexadecimal value
-        int first_value = hexaToDecimal(first_hexa_value);
-        int second_value = hexaToDecimal(second_hexa_value);
-        int result = first_value / second_value;
-        String result_hexa = decimalToHexa(result);
-        display(result_hexa);
+    /*
+     * divide two hexadecimal number
+     * @param first hexadecimal number
+     * @param second hexadecimal number
+     */
+    void div(String firsthexavalue, String secondhexavalue) { //divide to hexadecimal value
+        int firstvalue = hexaToDecimal(firsthexavalue);
+        int secondvalue = hexaToDecimal(secondhexavalue);
+        int result = firstvalue / secondvalue;
+        String resulthexa = decimalToHexa(result);
+        display(resulthexa);
     }
 
-    void isHexaEqual(String first_hexa_value, String second_hexa_value) { //check whether two hexadecimal values are equal or not
-        if (first_hexa_value.compareTo(second_hexa_value) == 0) {
+    /*
+     * to check whether two hexadecimal number are equal or not
+     * @param first hexadecimal number
+     * @param second hexadecimal number
+     */
+    void isHexaEqual(String firsthexavalue, String secondhexavalue) { //check whether two hexadecimal values are equal or not
+        if (firsthexavalue.compareTo(secondhexavalue) == 0) {
             displayBoolean(true);
         } else {
             displayBoolean(false);
         }
     }
 
-    void isHexaFirstGreater(String first_hexa_value, String second_hexa_value) { //check whether first hexadecimal is greater or not
-        if (first_hexa_value.compareTo(second_hexa_value) > 0) {
+    /*
+     * to check whether first hexadecimal number is greater then second or not
+     * @param first hexadecimal number
+     * @param second hexadecimal number
+     */
+    void isHexaFirstGreater(String firsthexavalue, String secondhexavalue) { //check whether first hexadecimal is greater or not
+        if (firsthexavalue.compareTo(secondhexavalue) > 0) {
             displayBoolean(true);
         } else {
             displayBoolean(false);
         }
     }
 
-    void isHexaSecondGreater(String first_hexa_value, String second_hexa_value) { //check whether second hexadecimal is greater or not
-        if (first_hexa_value.compareTo(second_hexa_value) < 0) {
+    /*
+     * to check whether second hexadecimal number is greater then first or not
+     * @param first hexadecimal number
+     * @param second hexadecimal number
+     */
+    void isHexaSecondGreater(String firsthexavalue, String secondhexavalue) { //check whether second hexadecimal is greater or not
+        if (firsthexavalue.compareTo(secondhexavalue) < 0) {
             displayBoolean(true);
         } else {
             displayBoolean(false);
         }
     }
 
+    /*
+     * display result such as add,sub,divide,multiply
+     * @param resulted value after applying operation
+     */
     void display(String result) { //display operation result such as add,sub,divide,multiply
         System.out.println("Result : " + result);
         
     }
 
-    void displayBoolean(boolean result) { //display boolean result
+    /*
+     * display boolean result
+     * @param resulted value after applying checking
+     */
+    void displayBoolean(boolean result) { //
         System.out.print("Result : " + result);
     }
 
     public static void main(String ard[]) {
         System.out.print("Enter first hexa number : ");
-        String hex_value_first = sc.next();
+        String firsthexavalue = sc.next();
         System.out.print("Enter second hexa number : ");
-        String hex_value_second = sc.next();
-        new Hexadecimal(hex_value_first, hex_value_second);
+        String secondhexavalue = sc.next();
+        new Hexadecimal(firsthexavalue, secondhexavalue);
 
     }
 }
