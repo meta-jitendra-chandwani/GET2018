68c9bc89945727b7839a83dde99bd73ddfb111e9
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-07-23 18:36:14 +0530

    Deleted

--- Assignment/Assignment1/Demo_Cart/ShopCart/ProductDetail.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-
-public class ProductDetail {
-	ArrayList<Object> product=new ArrayList<>();
-
-}
--- Assignment/New folder/ShopCart/src/Checkout.java
+++
@@ -1,9 +0,0 @@
-
-public class Checkout {
-	void promotion(){
-		
-	}
-	void finalBill() throws Exception {	// to generate the bill
-       
-    }
-}
