d168a51a0a1711663c7bd85667bc4c38e878bac5
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-07-25 16:52:11 +0530

    Parameterized test case applied

--- Assignment/Assignment5/LCMHCF/LcmHcfTest.java
+++ Assignment/Assignment5/LCMHCF/LcmHcfTest.java
@@ -1,17 +1,39 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-
+@RunWith(Parameterized.class)
 public class LcmHcfTest {
-
-	@Test
-	public void test() {
-		LcmHcf lcm=new LcmHcf();
-		int actual=lcm.hcf(6,9);
-		assertEquals(3, actual);
-		int actuallcm=lcm.lcm(6, 9);
-		assertEquals(18, actuallcm);
+private LcmHcf lcm;
+private int number1;
+private int number2;
+private int result;
+	 @Before
+		public void initializeObject() {
+		 lcm=new LcmHcf();
+		}
+	 
+	 public LcmHcfTest(int number1,int number2,int result) {
+		 this.number1=number1;
+		 this.number2=number2;	
+		 this.result=result;
 	}
-
+	
+	 @Parameters
+	 public static Collection<Object[]> maxMirror() {
+			return Arrays.asList(new Object[][] { { 6,9,18 } });
+		}
+	
+	 @Test
+		public void lcmTest() {
+			assertEquals(lcm.lcm(number1, number2),result);
+			assertNotEquals(lcm.hcf(number1, number2), result);
+		}
 }
