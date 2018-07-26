b9c2e77520ac9abf77ee244b4521a2e9f2995202
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-07-26 14:52:45 +0530

    Assignment6\Polynomial\Poly

--- Assignment/Assignment6/Polynomial/Poly.java
+++ Assignment/Assignment6/Polynomial/Poly.java
@@ -2,32 +2,92 @@ package Polynomial;
 
 import java.util.Scanner;
 
-public class Poly {
-	 static int Polynomial[]=new int[4];
-	 static int revPoly[]=new int[4];
-	 static Scanner input=new Scanner(System.in);
-	float evaluate(float value) {
-		float sum=0;
-		for(int i=0;i<Polynomial.length;i++){
-			sum+=revPoly[i]*Math.pow(value, i);
-		}
-			return sum;
-		
+/*
+ * Class Poly - Operation on polynomial
+ * evaluate value of polynomial
+ * Degree of polynomial
+ * Addition of two polynomials
+ * Multiplication of two polynomials 
+ */
+public class Poly implements PolyInterface {
+	int order;
+	static Scanner input = new Scanner(System.in);
+	double[] coeff;
+
+	/*
+	 * Parameterized constructor -  which assign value to coefficient and order.
+	 * @param coefficient array of polynomial
+	 */
+	Poly(double coeff[]) {
+		this.coeff = coeff;
+		this.order = coeff.length-1;
 	}
 	
-	public static void main(String args[]){
-		Poly poly=new Poly();
-		System.out.println("Enter Poly length : ");
-		int l=input.nextInt();
-		System.out.println("Enter value : ");
-		for(int i=0;i<l;i++){
-			Polynomial[i]=input.nextInt();
+	/*
+	 * (non-Javadoc)
+	 * @see Polynomial.PolyInterface#evaluate(float)
+	 * evaluate the value of polynomial
+	 * @param value of variable
+	 * @return total sum of polynomial
+	 */
+	@Override
+	public float evaluate(float value) {
+		float sum = 0;
+		for (int i = 0; i <= order; i++) {
+			sum += coeff[i] * Math.pow(value, order - i);
 		}
-		
-		for(int j=l-1;j>=0;j--){
-			revPoly[j]=Polynomial[j];
+		return sum;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see Polynomial.PolyInterface#degree()
+	 * Evaluate the degree of polynomial
+	 * @return Degree or Order or polynomial
+	 */
+	@Override
+	public int degree() {
+		return (order);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see Polynomial.PolyInterface#additionPoly(Polynomial.Poly, Polynomial.Poly)
+	 * Addition of two polynomials
+	 * @param polynomial 1
+	 * @param polynomial 2
+	 * @return sum of two polynomial
+	 */
+	@Override
+	public double[] additionPoly(Poly p1, Poly p2) {
+		int length;
+		if (p1.coeff.length > p2.coeff.length) {
+			length = p1.coeff.length;
+		} else {
+			length = p2.coeff.length;
 		}
-		System.out.println("Enter X value : ");
-		System.out.println(poly.evaluate(input.nextFloat()));
+		double result[] = new double[length];
+		for (int i = 0; i < p1.coeff.length; i++) {
+			result[i] = p1.coeff[i] + p2.coeff[i];
+		}
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see Polynomial.PolyInterface#multiplicationPoly(Polynomial.Poly, Polynomial.Poly)
+	 * Multiplication of two polynomials
+	 * @param polynomial 1
+	 * @param polynomial 2
+	 * @return multiplication of two polynomial	 */
+	@Override
+	public double[] multiplicationPoly(Poly p1, Poly p2) {
+		int totalLength = p1.coeff.length + p2.coeff.length - 1;
+		double[] result = new double[totalLength];
+		for (int i = 0; i < p1.coeff.length; i++)
+			for (int j = 0; j < p2.coeff.length; j++) {
+				result[i + j] += p1.coeff[i] * p2.coeff[j];
+			}
+		return result;
 	}
 }
