1e34a62ffebdbe2405b0fdbffe80ef80594942e1
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-07-26 14:50:21 +0530

    Add comments in HCFTest file

--- Assignment/Assignment5/LCMHCF/HcfTest.java
+++ Assignment/Assignment5/LCMHCF/HcfTest.java
@@ -11,29 +11,47 @@ import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
 public class HcfTest {
-private LcmHcf lcm;
-private int number1;
-private int number2;
-private int result;
-	 @Before
-		public void initializeObject() {
-		 lcm=new LcmHcf();
-		}
-	 
-	 public HcfTest(int number1,int number2,int result) {
-		 this.number1=number1;
-		 this.number2=number2;	
-		 this.result=result;
+	private LcmHcf lcm;
+	private int number1;
+	private int number2;
+	private int result;
+
+	/*
+	 * Initialize the object
+	 */
+	@Before
+	public void initializeObject() {
+		lcm = new LcmHcf();
 	}
 	
-	 @Parameters
-	 public static Collection<Object[]> maxMirror() {
-			return Arrays.asList(new Object[][] { { 6,9,3 } });
-		}
-	
-	 @Test
-		public void lcmTest() {
-			assertEquals(lcm.hcf(number1, number2),result);
-			
-		}
+	/*
+	 * Parameterized Constructor - initialize the variable
+	 * @param number1 - first number
+	 * @param number2 - second number
+	 * @pram result - resultant value
+	 */
+	public HcfTest(int number1, int number2, int result) {
+		this.number1 = number1;
+		this.number2 = number2;
+		this.result = result;
+	}
+
+	/*
+	 * Collection of Object -  contained the parameterized test case
+	 * @return Collection Object
+	 */
+	@Parameters
+	public static Collection<Object[]> maxMirror() {
+		return Arrays.asList(new Object[][] { { 6, 9, 3 } });
+	}
+
+	/*
+	 * Test Case evaluation
+	 * Check whether actual value is same as expected value
+	 */
+	@Test
+	public void lcmTest() {
+		assertEquals(lcm.hcf(number1, number2), result);
+
+	}
 }
