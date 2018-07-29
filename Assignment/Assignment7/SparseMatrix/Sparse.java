c45120e7d95694bfbaab0ae7f332e290adb00fe7
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-07-29 19:52:08 +0530

    SparseMatrix/Sparse

--- Assignment/Assignment7/SparseMatrix/Sparse.java
+++ Assignment/Assignment7/SparseMatrix/Sparse.java
@@ -1,58 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sparsematrix;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.Scanner;
 
-/**
- *
- * @author Jitendra
- */
 public class Sparse {
 
-    private static Scanner input = new Scanner(System.in);
     int row, col;
 
+    /*
+     * Initialization of row and coloumn
+     */
     public Sparse(int row, int col) {
         this.row = row;
         this.col = col;
     }
 
+    /*
+     * Transpose the given matrix
+     * @param matrix - Sparse Matrix in form of row,column and value
+     * @return transposeMatrix -  Transpose of matrix
+     */
     public int[][] transposeMatrix(int matrix[][]) {
-        int value[][] = new int[matrix.length][matrix[0].length];
+        int transposeMatrix[][] = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
-            value[i][0] = matrix[i][1];
-            value[i][1] = matrix[i][0];
-            value[i][2] = matrix[i][2];
+            transposeMatrix[i][0] = matrix[i][1];
+            transposeMatrix[i][1] = matrix[i][0];
+            transposeMatrix[i][2] = matrix[i][2];
         }
-        return value;
+        return transposeMatrix;
     }
 
+    /*
+     * Addition of given two matrix
+     * @param matrixOne - Sparse Matrix 1
+     * @param matrixTwo - Sparse Matrix 2
+     * @return additionMatrix - Addition of 2 Sparse Matrix
+     */
     public int[][] addMatrices(int matrixOne[][], int matrixTwo[][]) {
-        int sum[][] = new int[row][col];
-        System.out.println(matrixOne.length);
+        int additionMatrix[][] = new int[row][col];
         if (matrixOne.length == matrixTwo.length) {
             for (int i = 0; i < matrixOne.length; i++) {
-                sum[matrixOne[i][0]][matrixOne[i][1]] += matrixOne[i][2];
-                sum[matrixTwo[i][0]][matrixTwo[i][1]] += matrixTwo[i][2];
+                additionMatrix[matrixOne[i][0]][matrixOne[i][1]] += matrixOne[i][2];
+                additionMatrix[matrixTwo[i][0]][matrixTwo[i][1]] += matrixTwo[i][2];
             }
         }
-        return changetoSparseMatrix(sum, sum.length, sum[0].length);
+
+        return additionMatrix;
     }
 
+    /*
+     * Multiplication of given two matrix
+     * @param matrixOne - Sparse Matrix 1
+     * @param matrixTwo - Sparse Matrix 2
+     * @return multiplicationMatrix - Multiplication of 2 Sparse Matrix
+     */
     public int[][] multiplyMatrices(int matrixOne[][], int matrixTwo[][]) {
 
         List<Integer> Row = new ArrayList<>();
         List<Integer> Col = new ArrayList<>();
         List<Integer> Value = new ArrayList<>();
-//		if (matrixOne[0].length == matrixTwo.length) {
         for (int i = 0; i < matrixOne.length; i++) {
             for (int j = 0; j < matrixTwo.length; j++) {
                 if (matrixOne[i][1] == matrixTwo[j][0]) {
@@ -75,138 +82,51 @@ public class Sparse {
                 start = i;
             }
         }
-        int mul[][] = new int[Row.size()][3];
+        int multiplicationMatrix[][] = new int[Row.size()][3];
         for (int i = 0; i < Row.size(); i++) {
-            mul[i][0] = Row.get(i);
-            mul[i][1] = Col.get(i);
-            mul[i][2] = Value.get(i);
+            multiplicationMatrix[i][0] = Row.get(i);
+            multiplicationMatrix[i][1] = Col.get(i);
+            multiplicationMatrix[i][2] = Value.get(i);
         }
-//		}
-//        return mul;
-        return changetoSparseMatrix(mul, mul.length, mul[0].length);
+        return multiplicationMatrix;
+//        return changetoSparseMatrix(mul, mul.length, mul[0].length);
     }
 
-    boolean symmetrical(int transpose[][], int matrix[][], int matRow) {
+    /*
+     * Check whether given two matrix are equal or not
+     * @param matrixOne - Sparse Matrix 1
+     * @param matrixTwo - Sparse Matrix 2
+     * @return boolean flag value - True if two matrices are equal else false
+     */
+    boolean symmetrical(int matrixOne[][], int matrixTwo[][]) {
         boolean flag = true;
-        for (int i = 0; i < matRow; i++) {
-            if (!Arrays.equals(transpose[i], matrix[i])) {
+        for (int i = 0; i < matrixOne.length; i++) {
+            if (!Arrays.equals(matrixOne[i], matrixTwo[i])) {
                 flag = false;
             }
         }
         return flag;
     }
 
-    int[][] changetoSparseMatrix(int matrix[][], int matRow, int matCol) {
-
-        int newMatrix[][] = new int[row][col];
-        for (int i = 0; i < matRow; i++) {
-            newMatrix[matrix[i][0]][matrix[i][1]] = matrix[i][2];
-        }
-        return newMatrix;
-    }
-
-    public static void main(String arg[]) {
-//
-//        System.out.println("Enter number of row & column : ");
-//        int row = input.nextInt();
-//        int col = input.nextInt();
-//        System.out.println("Enter number of sparse value : ");
-//        int max = input.nextInt();
-//        int value[][] = new int[max][3];
-//
-//        System.out.println("Enter row colomn value of sparse matrix : ");
-//        int i = 0;
-//        for (int p = 0; p < max; p++) {
-//            System.out.println("Enter row column value :");
-//            for (int j = 0; j < 3 && i <= max; j++) {
-//                value[i][j] = input.nextInt();
-//            }
-//            i++;
-//
+//    int[][] changetoSparseMatrix(int matrix[][], int matRow) {
+//        int newMatrix[][] = new int[row][col];
+//        for (int i = 0; i < matrix.length; i++) {
+//            newMatrix[matrix[i][0]][matrix[i][1]] = matrix[i][2];
 //        }
-
-//        System.out.println("====Matrix 2====");
-//        System.out.println("Enter number of row & column : ");
-//        int row1 = input.nextInt();
-//        int col1 = input.nextInt();
-//        System.out.println("Enter number of sparse value : ");
-//        int max1 = input.nextInt();
-//        int value1[][] = new int[max1][3];
+//        return newMatrix;
+//    }
 //
-//        System.out.println("Enter row colomn value of sparse matrix : ");
+//    public static void main(String[] args) {
+//        Sparse sparse = new Sparse(4, 4);
+//        int setArray1[][] = {{0, 1, 1}, {0, 3, 2}, {1, 0, 3}, {1, 3, 4}, {2, 3, 5}, {3, 2, 6}};
+//        int setArray2[][] = {{0, 0, 3}, {0, 3, 4}, {1, 2, 4}, {2, 0, 6}, {2, 3, 5}, {3, 2, 1}};
 //
-//        int i1 = 0;
-//        for (int p = 0; p < max; p++) {
-//            System.out.println("Enter row column value :");
-//            for (int j = 0; j < 3 && i1 <= max; j++) {
-//                value1[i1][j] = input.nextInt();
-//            }
-//            i1++;
-//
-//        }
-//
-//        System.out.println("row\tcol\tvalue");
-//
-//        for (int k = 0; k < (i); k++) {
-//            for (int j = 0; j < 3; j++) {
-//                System.out.print(value[k][j] + "\t");
+//        int add[][] = sparse.addMatrices(setArray1, setArray2);
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                System.out.print(add[i][j] + "\t");
 //            }
 //            System.out.println();
 //        }
-//
-//        System.out.println("row\tcol\tvalue");
-//
-//        for (int k = 0; k < (i); k++) {
-//            for (int j = 0; j < 3; j++) {
-//                System.out.print(value1[k][j] + "\t");
-//            }
-//            System.out.println();
-//        }
-//
-        Sparse sparse = new Sparse(4, 4);
-////		
-//		System.out.println("Tanspose ======");
-//		int transposeMatrix[][] = sparse.transposeMatrix(value);
-//		for (int k = 0; k < (i); k++) {
-//			for (int j = 0; j < 3; j++) {
-//				System.out.print(transposeMatrix[k][j] + "\t");
-//			}
-//			System.out.println();
-//		}
-////
-////		System.out.println("Symmetric : "
-////				+ sparse.symmetrical(transposeMatrix, value, max));
-//
-//        int sumMatrix[][] = sparse.addMatrices(value, value1);
-//        System.out.println("Addition ======");
-//
-//        for (int k = 0; k < row; k++) {
-//            for (int j = 0; j < col; j++) {
-//                System.out.print(sumMatrix[k][j] + "\t");
-//            }
-//            System.out.println();
-//        }
-//
-//        int mulMatrix[][] = sparse.multiplyMatrices(value, value1);
-//        
-//        System.out.println("Multiplcation ======");
-////        for (int k = 0; k < mulMatrix.length; k++) {
-////            for (int j = 0; j < 3; j++) {
-////                System.out.print(mulMatrix[k][j] + "\t");
-////            }
-////            System.out.println();
-////        }
-        int setArray1[][] = {{0, 1, 1}, {0, 3, 2}, {1, 0, 3}, {1, 3, 4}, {2, 3, 5}, {3, 2, 6}};
-        int setArray2[][] = {{0, 0, 3}, {0, 3, 4}, {1, 2, 4}, {2, 0, 6}, {2, 3, 5}, {3, 2, 1}};
-
-        int mulpli[][] = sparse.multiplyMatrices(setArray1,setArray2);
-        for (int k = 0; k < (4); k++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print(mulpli[k][j] + "\t");
-            }
-            System.out.println();
-        }
-//        
-        }
-
-    }
+//    }
+}
