8692f1d946d5109a483dc90523704ca39178c25f
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-07-30 18:37:57 +0530

    Zone files

--- Assignment/Assignment8/Zoo_management_system/Zone/Zone.java
+++ Assignment/Assignment8/Zoo_management_system/Zone/Zone.java
@@ -1,18 +1,54 @@
-package zoo_management_system.Zone;
+package Zone;
 
 import java.util.ArrayList;
 import java.util.List;
-import zoo_management_system._CageEnum_;
+
+import Animal.BirdAnimal;
+import Animal.MammalAnimal;
+import Animal.ReptileAnimal;
+import Cage.Cage;
 
 public class Zone {
-    int CapacityZone=5;
-    List<String> cage=new ArrayList<>();
+    int CapacityZone=2;
+//    Cage cage=new Cage();
+    List<MammalAnimal> mammalInCage;//=Cage.getMammalCageList();
+    List<BirdAnimal> birdInCage;//=Cage.getBirdCageList();
+    List<ReptileAnimal> reptileInCage;//e=Cage.getReptileCageList();
+    public List<Cage> cageList=new ArrayList<Cage>();
+    Cage cage;
+
+    public Zone(_ZoneEnum_ zone) {
+    	if(_ZoneEnum_.Zone1.equals(zone)){
+    		cage=new Cage("mammal");
+    		mammalInCage=Cage.getMammalInCageList();
+    		cageList.add(new Cage("Mammal Cage1", "mammal", "Zone1"));
+    		cageList.add(new Cage("Mammal Cage2", "mammal", "Zone1"));
+    		
+    	}
+    	
+    	else if(_ZoneEnum_.Zone2.equals(zone)){
+    		cage=new Cage("bird");
+    		birdInCage=Cage.getBirdInCageList();
+    		cageList.add(new Cage("Bird Cage1", "Bird", "Zone3"));
+    		cageList.add(new Cage("Bird Cage2", "Bird", "Zone3"));
+    		
+    	}
+    	else if(_ZoneEnum_.Zone3.equals(zone)){
+    		cage=new Cage("reptile");
+    		reptileInCage=Cage.getReptileInCageList();
+    		cageList.add(new Cage("Reptile Cage1", "Reptile", "Zone2"));
+    		cageList.add(new Cage("Reptile Cage2", "Reptile", "Zone2"));
+    		
+    	}
+    	
+	}
 
     void getInfo(){
         
     }
     
     void addCage(){
+    	
         
     }
     
---
+++ Assignment/Assignment8/Zoo_management_system/Zone/_ZoneEnum_.java
@@ -0,0 +1,9 @@
+package Zone;
+
+public enum _ZoneEnum_ {
+	Zone1, // Mammal Zone
+	Zone2, // Mammal Zone
+	Zone3, // Reptile Zone
+//	Zone4, // Bird Zone
+
+}
