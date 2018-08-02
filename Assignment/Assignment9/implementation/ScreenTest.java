790f04ce879480688da68d5afb740d79194514d5
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-02 16:31:09 +0530

    Add commenting in ScreenTest

--- Assignment/Assignment9/implementation/ScreenTest.java
+++ Assignment/Assignment9/implementation/ScreenTest.java
@@ -6,10 +6,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Test;
-import implementation.Shape.ShapeType;
+import implementation.ShapeType;
 
 public class ScreenTest {
 
+	/*
+	 * testAddShape - function to add shape on screen
+	 */
 	@Test
 	public void testAddShapes() {
 		Screen.addShape(ShapeType.Circle, new Point(100, 200), new ArrayList<>(Arrays.asList(100d)));
