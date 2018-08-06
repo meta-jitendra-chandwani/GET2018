4c87e57178548d680db329239aa349f8236a861f
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-06 14:53:19 +0530

    Infix to postfix

---
+++ DSA/Assignment2/InfixToPostfix/CharacterStack.java
@@ -0,0 +1,11 @@
+package Assignment2;
+
+/*
+ * interface of Stack functionality
+ */
+public interface CharacterStack {
+	public String peek();
+	public void push (String item);
+	public String pop ();
+
+}
