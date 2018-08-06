35ddac842d7d9382243faf122e547e041eb5cc5c
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-06 16:24:07 +0530

    Nested Class - Operation on list

--- DSA/Assignment2/NestedList/NestedListClass.java
+++ DSA/Assignment2/NestedList/NestedListClass.java
@@ -1,60 +1,120 @@
-
-
 import java.util.ArrayList;
 import java.util.List;
-
+/*
+ * NestedListClass - implements NestedList, and perform operation on nestedlist.
+ */
 public class NestedListClass implements NestedList {
 
+	/*
+	 * (non-Javadoc)
+	 * @see NestedList#sumOfList(java.util.List)
+	 * sumOfList - To calculate the sum of nested list
+	 * @param nestedList - nested List
+	 * @return - the sum of all integer in nested list
+	 */
 	@Override
-	public int sumOfList(List<List<Integer>> list) {
+	public int sumOfList(List<List<Integer>> nestedList) {
 		int returnValue = 0;
-		if (list == null || list.size() == 0) {
-			returnValue = 0;
-		} else {
+		if (!(nestedList == null || nestedList.size() == 0)) {
 			int sum = 0;
-			for (List<Integer> nestedlist : list) {
-				for (Integer listValue : nestedlist) {
-					sum += listValue;
+			for (Object list : nestedList) {
+				if (list instanceof Integer) {
+					sum += (int) list;
+				} else {
+					sum += sumOfList((List) list);
 				}
 			}
 			returnValue = sum;
 		}
 		return returnValue;
+
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see NestedList#largestValue(java.util.List)
+	 * largestValue - To calculate the largest value from the nested list
+	 * @param nestedList - nested List
+	 * @return - the largest value from the nested list
+	 */
 	@Override
-	public int largestValue(List<Integer> list) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int largestValue(List<List<Integer>> nestedList) {
+		int returnValue = 0;
+		if (!(nestedList == null || nestedList.size() == 0)) {
+			int maximumValueInList = 0;
+			for (Object list : nestedList) {
+				if (list instanceof Integer) {
+					if ((int) list > maximumValueInList) {
+						maximumValueInList = (int) list;
+					}
+				} else {
+					maximumValueInList = largestValue((List) list);
+				}
+			}
+			returnValue = maximumValueInList;
+		}
+		return returnValue;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see NestedList#searchElement(java.util.List, int)
+	 * searchElement - Search the element in the nested list
+	 * @param nestedList - nested list
+	 * @param element - the element to be search in the list
+	 * @return - boolean value - true if element is search else false.
+	 */
 	@Override
-	public boolean searchElement(List<Integer> list, int element) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean searchElement(List<List<Integer>> nestedList, int element) {
+		boolean returnValue = false;
+		if (!(nestedList == null || nestedList.size() == 0)) {
+			for (Object list : nestedList) {
+				if (list instanceof Integer) {
+					if ((int) list == element) {
+						returnValue = true;
+						break;
+					}
+				} else {
+					if (!returnValue) {
+						returnValue = searchElement((List) list, element);
+					}
+				}
+			}
+		}
+		return returnValue;
 	}
 
-	public static void main(String []arg){
+	public static void main(String[] arg) {
 
 		List<List<Integer>> list = new ArrayList<>();
 		List<Integer> nestedList = new ArrayList<Integer>();
 		List<Integer> nestedList1 = new ArrayList<Integer>();
+		List<Integer> nestedList2 = new ArrayList<Integer>();
 
-		nestedList.add(4);
 		nestedList.add(1);
 		nestedList.add(2);
 		nestedList.add(3);
+		nestedList.add(4);
 
-		nestedList1.add(4);
 		nestedList1.add(5);
-		nestedList1.add(2);
-		nestedList1.add(3);
-
+		nestedList1.add(6);
+		nestedList1.add(7);
+		nestedList1.add(8);
+		
+		
+		nestedList2.add(9);
+		nestedList2.add(10);
+		nestedList2.add(11);
+		nestedList2.add(12);
+		
 		list.add(nestedList);
 		list.add(nestedList1);
-		
-		NestedListClass class1 = new NestedListClass();
-		System.out.println("Sum : " + class1.sumOfList(list));
+		list.add(nestedList2);
+
+		NestedListClass operation = new NestedListClass();
+		System.out.println("Sum of the lists : " + operation.sumOfList(list));
+		System.out.println("largest value from the list : " + operation.largestValue(list));
+		System.out.println("Search 11  from the list: " + operation.searchElement(list, 11));
 
 	}
 
