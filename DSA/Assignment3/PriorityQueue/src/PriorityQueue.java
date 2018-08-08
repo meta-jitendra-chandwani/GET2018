98792ba0cfa3d1771a4d17a11e698a2fcc44cc78
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-08 15:03:00 +0530

    Priority Queue

---
+++ DSA/Assignment3/PriorityQueue/src/PriorityQueue.java
@@ -0,0 +1,9 @@
+public class PriorityQueue {
+	String Name;
+	int Priority;
+
+	public PriorityQueue(String Name, int Priority) {
+		this.Name = Name;
+		this.Priority = Priority;
+	}
+}
---
+++ DSA/Assignment3/PriorityQueue/src/PriorityQueueInterface.java
@@ -0,0 +1,8 @@
+public interface PriorityQueueInterface<T> {
+
+	void insert(PriorityQueue object);
+
+	int getPriority();
+
+	void remove(PriorityQueue object);
+}
