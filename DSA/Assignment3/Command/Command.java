8dd7bb49ad9fe8c12a1803b30aae5993277d232f
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-08 23:51:55 +0530

    VCP

---
+++ DSA/Assignment3/Command/Command.java
@@ -0,0 +1,17 @@
+package Command;
+public enum Command {
+
+	/**
+	 * root - Root directory
+	 * mkdir - make new directory
+	 * cd - Change directory
+	 * ls - list
+	 * bk - back
+	 * find - find path
+	 * tree - structure of directory
+	 * exit - exit from VCM
+	 * cwd - current Working Directory
+	 */
+	root, mkdir, cd, bk, ls, find, tree, exit, cwd;
+
+}
