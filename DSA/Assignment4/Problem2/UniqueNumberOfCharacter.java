8aa255f8f138ee5fdce0cb8a1d195d3eeb934acb
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-09 18:25:12 +0530

    Commenting in Problem 2

--- DSA/Assignment4/Problem2/UniqueNumberOfCharacter.java
+++ DSA/Assignment4/Problem2/UniqueNumberOfCharacter.java
@@ -8,30 +8,25 @@ public class UniqueNumberOfCharacter {
 	int getNumberfCharacter(String givenString) {
 		HashMap<String, Integer> mapOfWords = new HashMap<String, Integer>();
 		String token[] = givenString.split(" ");
-		String temp = "";
-		int numberInMap=0;
-		int tokenLength = 0;
-		int number = 0;
-		for (int i = 0; i < token.length; i++) {
-			
-			numberInMap=0;
-			for (int j = 0; j < token[i].length(); j++) {
-				tokenLength+=token[i].length();
-				if(mapOfWords.containsKey(token[i])){
-					number+=0;
+		String emptyString = "";
+		int numberOfUniqueCharacterInMap = 0;
+		int countOfUniqueCharacterInString = 0;
+		for (int wordPostion = 0; wordPostion < token.length; wordPostion++) {
+
+			numberOfUniqueCharacterInMap = 0;
+			for (int characterPosition = 0; characterPosition < token[wordPostion].length(); characterPosition++) {
+
+				if (mapOfWords.containsKey(token[wordPostion])) {
+					break;
+				} else if (emptyString.indexOf(token[wordPostion].charAt(characterPosition)) == -1) {
+					emptyString = emptyString + token[wordPostion].charAt(characterPosition);
+					numberOfUniqueCharacterInMap++;
+					countOfUniqueCharacterInString++;
 				}
-				else if(temp.indexOf(token[i].charAt(j)) == -1 ){
-		             temp = temp + token[i].charAt(j);
-		             numberInMap++;
-		             number++;
-		             
-		         }
 			}
-		
-			mapOfWords.put(token[i], numberInMap);
-		
+			mapOfWords.put(token[wordPostion], numberOfUniqueCharacterInMap);
 		}
-		return number;
+		return countOfUniqueCharacterInString;
 	}
 
 	public static void main(String[] arg) {
