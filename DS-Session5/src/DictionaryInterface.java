c4b0290c0cc0f9a3a9e40d89ebe023ff6df4c567
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-16 14:38:19 +0530

    Deleted File

--- DS-Session5/src/DictionaryInterface.java
+++
@@ -1,9 +0,0 @@
-
-public interface DictionaryInterface{
-
-	boolean add(String key,String value);
-	boolean delete(String key);
-	boolean get(String key);
-	String sortedList();
-	String sortedListBetween(String key1,String key2);
-}
