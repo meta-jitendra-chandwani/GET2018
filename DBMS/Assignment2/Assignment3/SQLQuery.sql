7ea96d3f4c0948e69ccbd435601776e291c17eed
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-17 18:53:11 +0530

    Image query update

--- DBMS/Assignment2/Assignment3/SQLQuery.sql
+++ DBMS/Assignment2/Assignment3/SQLQuery.sql
@@ -1,4 +1,4 @@
-SELECT Order_Id, Order_Date, Order_Total FROM Orders;
+SELECT Order_Id, Order_Date, Order_Total FROM Orders WHERE (SELECT DATEDIFF(curdate(), Order_Date) AS Diff) >= 1;
 
 SELECT Order_Id, Order_Date, Order_Total FROM Orders ORDER BY Order_Total DESC LIMIT 10; 
 
@@ -8,7 +8,7 @@ SELECT * FROM User WHERE User_Type="Shopper" && (SELECT DATEDIFF(curdate(), Last
 
 SELECT U.User_Name,O.Order_Id FROM Orders as O INNER JOIN  User AS U ON O.User_Id=U.User_Id WHERE (SELECT DATEDIFF(curdate(), U.Last_Order) AS Diff) >= 15;
 
-SELECT P.Product_Title FROM Order_Product AS OP INNER JOIN Product AS P INNER JOIN Orders as O ON OP.Product_Id=P.Product_Id && OP.Order_Id=O.Order_Id WHERE O.Status="Shipped"  && O.Order_Id like "Order08%";
+SELECT P.Product_Title FROM Order_Product AS OP INNER JOIN Product AS P INNER JOIN Orders as O ON OP.Product_Id=P.Product_Id && OP.Order_Id=O.Order_Id WHERE O.Status="Shipped"  && O.Order_Id like "Order08/1";
 
 SELECT P.Product_Title,O.Order_Date FROM Order_Product AS OP INNER JOIN Product AS P INNER JOIN Orders as O ON OP.Product_Id=P.Product_Id && OP.Order_Id=O.Order_Id WHERE P.Price > 1000 && P.Price < 2000;
 
