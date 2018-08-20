c0aed191c615e235512d4f480f291d3a52de2430
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-20 17:29:47 +0530

    Assignment3/Assignment4

--- DBMS/Assignment3/Assignment4/ZipCode.sql
+++ DBMS/Assignment3/Assignment4/ZipCode.sql
@@ -1,3 +1,11 @@
+/**
+Create appropriate tables and relationships for the same and write a SQL
+query for that returns a Resultset containing Zip Code, City Names and
+States ordered by State Name and City Name.
+
+Create Table and show table data in Order by State name and City Name
+**/
+
 DROP Table Zip_Code;
 CREATE TABLE Zip_Code(
     ZipCode INT NOT NULL,
