ea55eeff5c463660ce819b00e8441245ae245056
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-20 18:00:43 +0530

    Assignment3/Assignment1

--- DBMS/Assignment3/Assignment1/SQLQuery.sql
+++ DBMS/Assignment3/Assignment1/SQLQuery.sql
@@ -1,3 +1,5 @@
+SELECT P.Product_Id,P.Product_Title,count(C.Category_Id) AS Count FROM Product AS P INNER JOIN Category AS C ON P.Category_Id=C.Category_Id GROUP BY C.Category_Id HAVING Count>1 ORDER BY Count DESC;
+
 SELECT COUNT(*) FROM Product WHERE Price < 1000 && Price > 0;
 SELECT COUNT(*) FROM Product WHERE Price < 5000 && Price > 1001;
 SELECT COUNT(*) FROM Product WHERE Price > 5001;
