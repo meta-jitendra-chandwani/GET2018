2540e75c2eac1fc1de816f4cdbea2838f30efed6
meta-jitendra-chandwani <jitendra.chandwani@metacube.com>
2018-08-29 13:56:43 +0530

    Session 2

--- UI_UX/HTML_Session_2/validation.js
+++ UI_UX/HTML_Session_2/validation.js
@@ -1,17 +1,20 @@
-function nameValidation() {
+/**
+ * nameValidation - Validation of first and last name text input
+ * using regex - which only accept A-Z and a-z as input in text field
+ * check whether the length of input is greater than 2 and sastisfy regex or not.
+ */
+function nameValidation(Value) {
     var regex = /^[A-Z,a-z]+$/
-    var checkName = document.getElementById("first_name").value;
-    var erromessage = document.getElementById("first_name_error");
+    var checkName = document.getElementById(Value.id).value;
+    var erromessage = document.getElementById(Value.id+"_error");
     checkName.length > 2 && regex.test(checkName) ? erromessage.style.visibility = "hidden" : erromessage.style.visibility = "visible";
 }
 
-function lastNameValidation() {
-    var regex = /^[A-Z,a-z]+$/;
-    var checkName = document.getElementById("last_name").value;
-    var erromessage = document.getElementById("last_name_error");
-    checkName.length > 2 && regex.test(checkName) ? erromessage.style.visibility = "hidden" : erromessage.style.visibility = "visible";
-}
-
+/**
+ * mailValidation - Validation of email text input
+ * using regex - which only accept email as input in text field
+ * check whether input sastisfy regex or not.
+ */
 function mailValidation() {
     var regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     var email = document.getElementById("email_id").value;
@@ -19,6 +22,11 @@ function mailValidation() {
     regex.test(email) ? erromessage.style.visibility = "hidden" : erromessage.style.visibility = "visible";
 }
 
+/**
+ * validatePassword - Validation of passowrd input
+ * using regex - which only accept atleast 1 Captial 1 small alphabet and 1 number and special character as input in text field
+ * check whether input sastisfy regex or not.
+ */
 function validatePassword() {
     var password = document.getElementById("password_id").value;
     var confirmPassword = document.getElementById("password_repeat_id").value;
@@ -42,12 +50,19 @@ function validatePassword() {
         passwordDoesntMatch.style.visibility = "visible";
     }
 }
-
+/**
+ * showPassword - show password that user enter in text format
+ */
 function showPassword() {
     var showPassword = document.getElementById("password_id");
     showPassword.type === "password" ? showPassword.type = "text" : showPassword.type = "password";
 }
 
+/**
+ * validatePassword - Validation of passowrd input
+ * using regex - which only accept atleast 1 Captial 1 small alphabet and 1 number and special character as input in text field
+ * check whether input sastisfy regex or not.
+ */
 function validateContact() {
     var contact = document.getElementById("contact_id").value;
     var errormessage = document.getElementById("contact_id_error");
